b227b0d82ea091dff6d60ead71275f47f621d70b
Max Weiner <max.weiner@posteo.de>
2026-09-10 15:45:46 +0200

    fix: dependency of collect datasets on provisional directory node

--- ae_pipeline/aa_data/task_collect_datasets.py
+++ ae_pipeline/aa_data/task_collect_datasets.py
@@ -1,10 +1,14 @@
+from pathlib import Path
+from typing import Annotated
+
 import pytask
 
 from ae_pipeline.task_config import AA_DATA_DIR, CATALOG, Dataset
 
 
 def task_collect_datasets(
+    primary_files: Annotated[list[Path], pytask.DirectoryNode(root_dir=AA_DATA_DIR, pattern="*.pridb")],
     produces: pytask.PickleNode = CATALOG["datasets"],  # type: ignore
 ):
-    datasets = [Dataset(primary_file=f) for f in AA_DATA_DIR.glob("*.pridb")]
+    datasets = [Dataset(primary_file=f) for f in primary_files]
     produces.save(datasets)
